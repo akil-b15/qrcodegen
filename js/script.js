5ab2332ed92ba8bde2ffe66c945a2d8b990afd86
Akil Ahmed <akilahmed.b15@gmail.com>
2022-09-05 23:13:13 +0600

    save button fixed

--- js/script.js
+++ js/script.js
@@ -37,7 +37,9 @@ const generateQRCode = (url, size) => {
 
 const clearUI = () => {
     qr.innerHTML = '';
-}
+    const saveLink = document.getElementById('save-link');
+    if (saveLink) saveLink.remove();
+};
 
 const showSpinner = () => {
     document.getElementById('spinner').style.display = 'block';
@@ -50,7 +52,7 @@ const hideSpinner = () => {
 const createSaveBtn = (saveUrl) => {
     const link = document.createElement('a');
     link.id = 'save-link';
-    link.classList = 'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
+    link.classList = 'bg-blue-400 hover:bg-blue-600 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
     link.href = saveUrl;
     link.download = 'qrcode';
     link.innerHTML = 'Save Image';
